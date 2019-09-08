242be131118228fa23491e5daeac3d06f055ce9f
KB <kedar_bas@hotmail.com>
2019-09-08 17:26:24 -0500

    kernelcall

--- src/bin/kernelcall.rs
+++ src/bin/kernelcall.rs
@@ -1,3 +1,11 @@
+extern crate libc;
+
+
 fn main() {
-    
+    //pub unsafe extern fn getpid() -> pid_t
+
+    unsafe {
+        println!("{}", libc::getpid());
+        println!("{}", libc::gettimeofday()");
+    }
 }
