d1996af5153aa8f816eddf4154d98e8da4a16440
Kedar <kedar_bas@hotmail.com>
2019-09-19 16:38:28 -0500

    Cleaning

--- src/bin/command.rs
+++ src/bin/command.rs
@@ -6,11 +6,11 @@ fn main() {
     // Execute `ls` in the current directory of the program.
     list_dir.status().expect("process failed to execute");
 
-    println!("");
+    println!("Should be running");
 
     // Change `ls` to execute in the root directory.
-    list_dir.current_dir("/");
+    //list_dir.current_dir("/");
 
     // And then execute `ls` again but in the root directory.
-    list_dir.status().expect("process failed to execute");
+    //list_dir.status().expect("process failed to execute");
 }
