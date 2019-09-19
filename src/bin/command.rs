a31643079154c96798005682ade808b495faf812
Kedar <kedar_bas@hotmail.com>
2019-09-19 17:24:36 -0500

    Cleaning

--- src/bin/command.rs
+++ src/bin/command.rs
@@ -8,7 +8,9 @@ fn main() {
     // .stderr(Stdio::null())
     // .spawn();
 
-    let mut server = Command::new ("./target/debug/us").status().expect("Unable to start server");
+    let mut server = Command::new ("./target/debug/us")
+    .arg("lat")
+    .status().expect("Unable to start server");
 
     // Execute `ls` in the current directory of the program.
     //list_dir.status().expect("process failed to execute");
