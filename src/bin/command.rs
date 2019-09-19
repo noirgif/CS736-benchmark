48e12732647a888d3a3b4d041c2bc410fb80f4a3
Kedar <kedar_bas@hotmail.com>
2019-09-19 17:19:21 -0500

    Cleaning

--- src/bin/command.rs
+++ src/bin/command.rs
@@ -1,6 +1,7 @@
 use std::process::{Command, Stdio};
 
 fn main() {
+    println!("Starting Tests ..."));
     // let mut client = Command::new("./target/debug/uc lat")
     // .stderr(Stdio::null())
     // .stdout(Stdio::null())
