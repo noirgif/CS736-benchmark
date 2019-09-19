7596f1241d3adc047d6160c06ecd226447432f5e
noirgif <nomaru@outlook.com>
2019-09-19 03:14:30 -0500

    Modify kernel calls

--- src/kernelcall.rs
+++ src/kernelcall.rs
@@ -1,3 +1,6 @@
+#![feature(asm)]
+#![allow(unused)]
+
 extern crate libc;
 use std::time::Duration;
 extern crate core;
@@ -6,11 +9,19 @@ use core::arch::x86_64::__rdtscp;
 #[macro_use]
 mod measure;
 
+
 fn main() {
+    // TSC Frequency in GHz
+    const TSC_FREQ : f64 = 3.2;
+    let mut pid : libc::pid_t = -1;
+    let cyc_getpid = rdtscp!({unsafe {asm!("syscall" : "={rax}"(pid) : "{rax}"(39) :: "intel", "volatile")}}, 100000);
+    println!("{}", cyc_getpid as f64 / TSC_FREQ);
 
-    let cyc = rdtscp!({unsafe {libc::getuid();}}, 100000);
-    println!("{}", cyc);
+    // println!("{}", pid);
 
-    let lat = gettime!({unsafe {libc::getuid();}}, 100000);
-    println!("rdtscp diff = {}ns", lat);
+    let mut uid : libc::uid_t = 0xFFFF;
+    let cyc_getuid = rdtscp!({unsafe {asm!("syscall" : "={rax}"(uid) : "{rax}"(102) :: "intel", "volatile")}}, 100000);
+    println!("{}", cyc_getuid as f64 / TSC_FREQ);
+
+    //println!("{}", uid);
 }
