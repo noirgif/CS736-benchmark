acfa8a21de0661e30fad8bcc840a22c48d163352
Kedar <kedar_bas@hotmail.com>
2019-09-19 18:28:54 -0500

    Cleaning

--- src/bin/command.rs
+++ src/bin/command.rs
@@ -19,6 +19,18 @@ fn main() {
 
 
     // UDP Throughput
+    let client = Command::new("./target/debug/uc")
+    .arg("lat")
+    .arg("1000000")
+    .stderr(Stdio::null())
+    .stdout(Stdio::null())
+    .stderr(Stdio::null())
+    .spawn();
+
+    let server = Command::new ("./target/debug/us")
+    .arg("lat")
+    .arg("1000000")
+    .status().expect("Unable to start server");
     
-    println!("\n\nSuccess all tests have concluded!\n\n");
+    println!("\n\nAll tests have concluded!\n\n");
 }
